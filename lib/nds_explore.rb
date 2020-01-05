7331731f1b022ef8c26b01ae05c3a4a32527792e
James Shipman <jbshipman@icloud.com>
2020-01-05 15:52:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,9 +11,9 @@ end
 
 def print_first_directors_movie_titles
   i = 0 
-  
-  while i < directors_database[0][:movies].length do 
-    puts directors_database[0][:movies][i][:title]
+  spileburg = directors_database[0]
+  while i < spileburg[:movies].length do 
+    puts sileburg[:movies][i][:title]
     i += 1 
   end 
   
